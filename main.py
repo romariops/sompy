715f02db1df6e66b1979524d99ca7cbad2c812d2
romariops <ifrnromariosilva@gmail.com>
2019-11-16 15:45:23 -0300

    funçao de pegar codigos das teclas do teclado

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.uix.button import Button
+from kivy.core.audio import SoundLoader
+
 
 class Tablet(BoxLayout):
     pass
 
+    
 class SomPyApp(App):
     def build(self):
+        Window.bind(on_key_down=self.on_keyboard_down)
         return Tablet()
+
+    def on_keyboard_down(self, window, keycode, *args):
+        print(keycode)
+        
 
 SomPyApp().run()
