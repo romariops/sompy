b74bf61333c6cf01dd1d1420e93b6d42b5ce6a46
romariops <ifrnromariosilva@gmail.com>
2019-11-22 12:07:18 -0300

    adcionada a funcao de troca de tela

--- main.py
+++ main.py
@@ -2,22 +2,27 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 from kivy.uix.button import Button
-from som import PlaySom
+from playsom import PlaySom
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class Tablet(BoxLayout):
-    def __init__(self):
-        super().__init__()
-    
+
+class Interface(ScreenManager):
+    pass
+
+class Tablet(Screen):
     def play_sample(self,keycode):
         play=PlaySom()
         play.play_samples(keycode)
+
+class SelectSamples(Screen):
+    pass
 
     
 class SomPyApp(App):
     
     def build(self):
         Window.bind(on_key_down=self.on_keyboard_down)
-        return Tablet()
+        return Interface()
 
     def on_keyboard_down(self, window, keycode, *args):
         play=PlaySom()
